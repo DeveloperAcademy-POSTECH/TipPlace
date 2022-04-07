0aaa413c76868cff3d1cb540d528ebcbcf6e91fc
ddophi98 <lightcloud98@gmail.com>
2022-04-07 22:46:01 +0900

    wip: 태그 구현 진행중

--- TipPlace/TipPlace/PostUploadView.swift
+++ TipPlace/TipPlace/PostUploadView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 class GlobalVarInPostUploadView: ObservableObject {
     @Published var selectedCategory: String = "카테고리 선택"
+    @Published var tagSentence: String = ""
 }
 
 struct PostUploadView: View {
     @Environment(\.presentationMode) var presentationMode
     @ObservedObject var globalVar = GlobalVarInPostUploadView()
     @State var title: String = ""
-    @State var tag: String = ""
     @State var content: String = ""
     var body: some View {
         NavigationView {
@@ -36,13 +36,18 @@ struct PostUploadView: View {
                 ) { Text(self.globalVar.selectedCategory).padding(.horizontal, 8) }
                 // 태그 입력 칸
                 ZStack(alignment: .topLeading) {
-                    if tag.isEmpty {
+                    if globalVar.tagSentence.isEmpty {
                         Text("#태그는 5개까지 입력할 수 있어요")
                             .foregroundColor(Color(UIColor.placeholderText))
                             .padding(.horizontal, 8)
                             .padding(.vertical, 12)
                     }
-                    TextEditor(text: $tag).frame(height: 60)
+                    ScrollView {
+                        VStack {
+                            WrappedLayoutForTag()
+                        }
+                    }
+                    TextEditor(text: $globalVar.tagSentence).frame(height: 60)
                 }
                 // 게시글 입력 칸
                 ZStack(alignment: .topLeading) {
@@ -120,5 +125,55 @@ struct CategorySelectView: View {
 struct PostUploadView_Previews: PreviewProvider {
     static var previews: some View {
         PostUploadView()
+    }
+}
+
+struct WrappedLayoutForTag: View {
+    @State var platforms = ["Ninetendo", "XBox", "PlayStation", "PlayStation 2", "PlayStation 3"]
+
+    var body: some View {
+        GeometryReader { geometry in
+            self.generateContent(in: geometry)
+        }
+    }
+
+    private func generateContent(in geoProxy: GeometryProxy) -> some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+
+        return ZStack(alignment: .topLeading) {
+            ForEach(self.platforms, id: \.self) { platform in self.item(for: platform)
+                    .padding([.horizontal, .vertical], 4)
+                    .alignmentGuide(.leading, computeValue: { dens in
+                        if abs(width - dens.width) > geoProxy.size.width {
+                            width = 0
+                            height -= dens.height
+                        }
+                        let result = width
+                        if platform == self.platforms.last! {
+                            width = 0 // last item
+                        } else {
+                            width -= dens.width
+                        }
+                        return result
+                    })
+                    .alignmentGuide(.top, computeValue: { _ in
+                        let result = height
+                        if platform == self.platforms.last! {
+                            height = 0 // last item
+                        }
+                        return result
+                    })
+            }
+        }
+    }
+
+    func item(for text: String) -> some View {
+        Text(text)
+            .padding(.all, 5)
+            .font(.body)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
+            .cornerRadius(5)
     }
 }
