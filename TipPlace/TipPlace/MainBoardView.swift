cfe464206dd73c91ef3ae014e0870c206f5d492e
리아 <kjk0825@postech.ac.krl>
2022-04-08 18:52:49 +0900

    feat: add search bar & toolbar

--- TipPlace/TipPlace/MainBoardView.swift
+++ TipPlace/TipPlace/MainBoardView.swift
@@ -2,17 +2,32 @@
 //  MainBoardView.swift
 //  MC1_Team16_UNsIX_App
 //
-//  Created by 김수진 on 2022/04/05.
+//  Created by Lia on 2022/04/05.
 //
 
 import SwiftUI
 
 struct MainBoardView: View {
+    @State var searchText: String = ""
     var body: some View {
         NavigationView {
             ZStack {
                 Color.blue
-            }.navigationTitle("정보")
+            }
+            .navigationTitle("정보")
+            .searchable(text: $searchText, prompt: "정보를 검색해보세요") {
+                Text("apple").searchCompletion("apple")
+                Text("banana").searchCompletion("banana")
+                Text("orange").searchCompletion("orange")
+                Text("mango").searchCompletion("mango")
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: { print("writing..") }) {
+                        Image(systemName: "square.and.pencil")
+                    }
+                }
+            }
         }
     }
 }
