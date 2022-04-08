fd34df36b2df9c04d24def35c800b4b5f823c860
리아 <kjk0825@postech.ac.krl>
2022-04-08 22:05:03 +0900

    feat: add horizontal scrollable tag list

--- TipPlace/TipPlace/MainBoardView.swift
+++ TipPlace/TipPlace/MainBoardView.swift
@@ -2,17 +2,37 @@
 //  MainBoardView.swift
 //  MC1_Team16_UNsIX_App
 //
-//  Created by Lia on 2022/04/05.
+//  Created by 리아 on 2022/04/05.
 //
 
 import SwiftUI
 
 struct MainBoardView: View {
     @State var searchText: String = ""
+    var recommendedTags: [String] = load(fileName: "RecommendedTagData.json")
     var body: some View {
         NavigationView {
-            ZStack {
-                Color.blue
+            VStack {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(recommendedTags, id: \.self) { tag in
+                            Button(action: { print(tag) }) {
+                                Text(tag)
+                                    .padding(.vertical, 8)
+                                    .padding(.horizontal, 15)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.blue, lineWidth: 2)
+                                    )
+                            }
+                        }
+                        .padding(.vertical, 15)
+                        .padding(.leading, 10)
+                    }
+                }
+                .padding(.leading, 15)
+                Spacer()
+                Text("")
             }
             .navigationTitle("정보")
             .searchable(text: $searchText, prompt: "정보를 검색해보세요") {
