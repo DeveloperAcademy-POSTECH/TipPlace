54b195752a2dc2a43599b66fb0be2c865598cc78
dgfghsjd <dgfghsjd@naver.com>
2022-04-08 13:41:17 +0900

    fix: change button enum name

--- TipPlace/TipPlace/CategoryView.swift
+++ TipPlace/TipPlace/CategoryView.swift
@@ -38,20 +38,20 @@ struct CategorySelectButtonView: View {
 struct MockStore {
     static var buttons = [
         CategorySelectButton(id: 0, title: Category.economy.korean, imageUrl: "wonsign.circle", categoryEnum: Category.economy),
-        CategorySelectButton(id: 1, title: Category.law.korean, imageUrl: "text.book.closed", categoryEnum: Category.economy),
-        CategorySelectButton(id: 2, title: Category.safety.korean, imageUrl: "figure.walk.diamond", categoryEnum: Category.economy),
-        CategorySelectButton(id: 3, title: Category.cook.korean, imageUrl: "fork.knife", categoryEnum: Category.economy),
-        CategorySelectButton(id: 4, title: Category.livingAlone.korean, imageUrl: "house", categoryEnum: Category.economy),
-        CategorySelectButton(id: 5, title: Category.cleaning.korean, imageUrl: "wand.and.stars", categoryEnum: Category.economy),
-        CategorySelectButton(id: 6, title: Category.tech.korean, imageUrl: "hammer", categoryEnum: Category.economy),
-        CategorySelectButton(id: 7, title: Category.driving.korean, imageUrl: "car", categoryEnum: Category.economy),
-        CategorySelectButton(id: 8, title: Category.health.korean, imageUrl: "cross", categoryEnum: Category.economy),
-        CategorySelectButton(id: 9, title: Category.campusLife.korean, imageUrl: "books.vertical", categoryEnum: Category.economy),
-        CategorySelectButton(id: 10, title: Category.workingLife.korean, imageUrl: "network", categoryEnum: Category.economy),
-        CategorySelectButton(id: 11, title: Category.companionLife.korean, imageUrl: "pawprint", categoryEnum: Category.economy),
-        CategorySelectButton(id: 12, title: Category.hobby.korean, imageUrl: "lasso.and.sparkles", categoryEnum: Category.economy),
-        CategorySelectButton(id: 13, title: Category.relationship.korean, imageUrl: "person.2", categoryEnum: Category.economy),
-        CategorySelectButton(id: 14, title: Category.etc.korean, imageUrl: "arrow.triangle.branch", categoryEnum: Category.economy)
+        CategorySelectButton(id: 1, title: Category.law.korean, imageUrl: "text.book.closed", categoryEnum: Category.law),
+        CategorySelectButton(id: 2, title: Category.safety.korean, imageUrl: "figure.walk.diamond", categoryEnum: Category.safety),
+        CategorySelectButton(id: 3, title: Category.cook.korean, imageUrl: "fork.knife", categoryEnum: Category.cook),
+        CategorySelectButton(id: 4, title: Category.livingAlone.korean, imageUrl: "house", categoryEnum: Category.livingAlone),
+        CategorySelectButton(id: 5, title: Category.cleaning.korean, imageUrl: "wand.and.stars", categoryEnum: Category.cleaning),
+        CategorySelectButton(id: 6, title: Category.tech.korean, imageUrl: "hammer", categoryEnum: Category.tech),
+        CategorySelectButton(id: 7, title: Category.driving.korean, imageUrl: "car", categoryEnum: Category.driving),
+        CategorySelectButton(id: 8, title: Category.health.korean, imageUrl: "cross", categoryEnum: Category.health),
+        CategorySelectButton(id: 9, title: Category.campusLife.korean, imageUrl: "books.vertical", categoryEnum: Category.campusLife),
+        CategorySelectButton(id: 10, title: Category.workingLife.korean, imageUrl: "network", categoryEnum: Category.workingLife),
+        CategorySelectButton(id: 11, title: Category.companionLife.korean, imageUrl: "pawprint", categoryEnum: Category.companionLife),
+        CategorySelectButton(id: 12, title: Category.hobby.korean, imageUrl: "lasso.and.sparkles", categoryEnum: Category.hobby),
+        CategorySelectButton(id: 13, title: Category.relationship.korean, imageUrl: "person.2", categoryEnum: Category.relationship),
+        CategorySelectButton(id: 14, title: Category.etc.korean, imageUrl: "arrow.triangle.branch", categoryEnum: Category.etc)
     ]
 }
 
