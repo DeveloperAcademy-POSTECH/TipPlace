5f6d3abe000f49e9013e4b15717c3f0dc9feb304
PJunyeong <j_aion@naver.com>
2022-04-12 11:46:45 +0900

    WIP: boardPost 수정 중

--- TipPlace/TipPlace/ModelData/MockData.swift
+++ TipPlace/TipPlace/ModelData/MockData.swift
@@ -22,8 +22,7 @@ struct BoardPostListMock {
         BoardPostMock.boardPost54,
         BoardPostMock.boardPost55,
         BoardPostMock.boardPost56,
-        BoardPostMock.boardPost57,
-        BoardPostMock.boardPost58]
+        BoardPostMock.boardPost57]
 }
 
 struct AuthorMock {
@@ -41,6 +40,9 @@ struct AuthorMock {
     static var author56 = Author(id: 56, profileImage: nil, name: "산책좋아", specialDomain: nil)
     static var author57 = Author(id: 57, profileImage: nil, name: "수원집사", specialDomain: nil)
 }
+
+
+
 
 struct BoardPostMock {
     static var boardPost1 = BoardPost(id: 1,
@@ -103,7 +105,9 @@ struct BoardPostMock {
                                        createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 3, day: 25, hour: 20, minute: 20))!,
                                        thumnailImageUrl: URL(string: "https://static.dogmate.co.kr/blog/wp-content/uploads/2019/04/25165439/image_6552724361539941650988.jpg"),
                                        usefulCount: 6,
-                                       replyCount: 3)
+                                       replyCount: 3,
+                                       tag:["강아지", "힐링", "산책", "건강"],
+                                       category:Category.companionLife)
     static var boardPost54 = BoardPost(id: 54,
                                        isAnonymous: false,
                                        title: "강아지가 깨물때 대처법",
@@ -112,7 +116,9 @@ struct BoardPostMock {
                                        createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 1, hour: 20, minute: 12))!,
                                        thumnailImageUrl: URL(string: "https://t1.daumcdn.net/cfile/tistory/1154274F4F26BD861E"),
                                        usefulCount: 6,
-                                       replyCount: 3)
+                                       replyCount: 3,
+                                       tag:["강아지", "힐링", "양육 방법"],
+                                       category: Category.companionLife)
     static var boardPost55 = BoardPost(id: 55,
                                        isAnonymous: true,
                                        title: "고양이랑 친해지려면 어떻게 해야하나요?",
@@ -121,50 +127,56 @@ struct BoardPostMock {
                                        createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 1, hour: 21, minute: 34))!,
                                        thumnailImageUrl: nil,
                                        usefulCount: 3,
-                                       replyCount: 10)
+                                       replyCount: 10,
+                                       tag:["고양이", "집사", "갓냥이"],
+                                       category: Category.companionLife)
     static var boardPost56 = BoardPost(id: 56,
                                        isAnonymous: true,
-                                       title: "집에서 할만한 운동?",
-                                       content: "평소에 헬스장 열심히 가다가 코로나에 걸려서 집에서 격리하게 됐는데, 혹시 방안에서 할만한 운동이 있는지 조언을 듣고싶습니다.",
+                                       title: "자취생 집에서 할만한 운동?",
+                                       content: "평소에 헬스장 열심히 가다가 코로나에 걸려서 집에서 격리하게 됐는데, 혹시 방안에서 할만한 운동이 있는지 조언을 듣고싶습니다. 홈트 + 자취생 읽어주세요!!",
                                        author: AuthorMock.author54,
                                        createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 2, hour: 15, minute: 10))!,
                                        thumnailImageUrl: nil,
                                        usefulCount: 2,
-                                       replyCount: 2)
+                                       replyCount: 2,
+                                       tag:["운동", "자취", "홈트", "혼자운동"],
+                                       category: Category.livingAlone)
     static var boardPost57 = BoardPost(id: 57,
                                        isAnonymous: false,
-                                       title: "집에서 하기 좋은 운동 모음",
-                                       content: "요즘 코로나 때문에 헬스장 다니기도 힘드시죠?\n그런분들을 위해 집에서 간단하게 할 수 있는 맨몸 운동들을 알려주려 합니다.\n우선 제일 추천하는 것은 스쿼트입니다.\n참고로 스쿼트는 다리 뿐만 아니라 여러 근육을 강화할 수 있는 전신운동입니다.",
+                                       title: "자취생 집에서 하기 좋은 운동 모음",
+                                       content: "요즘 코로나 때문에 헬스장 다니기도 힘드시죠?\n그런분들을 위해 집에서 간단하게 할 수 있는 맨몸 운동들을 알려주려 합니다.\n우선 제일 추천하는 것은 스쿼트입니다.\n참고로 스쿼트는 다리 뿐만 아니라 여러 근육을 강화할 수 있는 전신운동입니다. 자취생 필독 ㄱㅅㄱㅅ",
                                        author: AuthorMock.author55,
                                        createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 2, hour: 20, minute: 01))!,
                                        thumnailImageUrl: nil,
                                        usefulCount: 10,
-                                       replyCount: 5)
-    static var boardPost58 = BoardPost(id: 58,
-                                       isAnonymous: true,
-                                       title: "운동으로 걷기, 달리기 중 어떤게 좋나요?",
-                                       content: "요즘 시간이 남아서 가볍게 운동을 시작해보려 합니다.\n마침 저희 집 앞에 긴 산책로도 있고 해서 걷기나 달리기를 해볼까 하는데 어떤 게 더 건강에 좋을까요?",
-                                       author: AuthorMock.author56,
-                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 3, hour: 11, minute: 57))!,
-                                       thumnailImageUrl: nil,
-                                       usefulCount: 2,
-                                       replyCount: 5)
-    static var boardPost59 = BoardPost(id: 59,
-                                       isAnonymous: false,
-                                       title: "걷기 vs 달리기",
-                                       content: "관련 글을 보게 되어서 정리를 하려 합니다.\n걷기와 달리기는 각각 특징이 있습니다.\n극단적으로 단점만 얘기하자면 걷기는 효율이 낮고 달리기는 몸에 무리가 갑니다.\n따라서 제가 추천하는 것은 빨리걷기 입니다.",
-                                       author: AuthorMock.author55,
-                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 3, hour: 15, minute: 10))!,
-                                       thumnailImageUrl: nil,
-                                       usefulCount: 8,
-                                       replyCount: 6)
-    static var boardPost60 = BoardPost(id: 60,
-                                       isAnonymous: false,
-                                       title: "고양이가 먹이를 안먹습니다ㅠㅠ",
-                                       content: "안녕하세요, 오늘 아침에 저희 고양이에게 먹이를 주는데 먹지를 않더라고요.. 평소에는 주자마자 허겁지겁 먹던 아이인데 이런 적은 처음이라서 당황스럽네요ㅠㅠㅠ 어디가 아픈걸까요? 혹시 이런 경험 있으신 분들 있을까요?",
-                                       author: AuthorMock.author57,
-                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 5, hour: 11, minute: 50))!,
-                                       thumnailImageUrl: nil,
-                                       usefulCount: 2,
-                                       replyCount: 4)
+                                       replyCount: 5,
+                                       tag:["자취", "운동", "홈트"],
+                                       category: Category.livingAlone)
+//    static var boardPost58 = BoardPost(id: 58,
+//                                       isAnonymous: true,
+//                                       title: "운동으로 걷기, 달리기 중 어떤게 좋나요?",
+//                                       content: "요즘 시간이 남아서 가볍게 운동을 시작해보려 합니다.\n마침 저희 집 앞에 긴 산책로도 있고 해서 걷기나 달리기를 해볼까 하는데 어떤 게 더 건강에 좋을까요?",
+//                                       author: AuthorMock.author56,
+//                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 3, hour: 11, minute: 57))!,
+//                                       thumnailImageUrl: nil,
+//                                       usefulCount: 2,
+//                                       replyCount: 5)
+//    static var boardPost59 = BoardPost(id: 59,
+//                                       isAnonymous: false,
+//                                       title: "걷기 vs 달리기",
+//                                       content: "관련 글을 보게 되어서 정리를 하려 합니다.\n걷기와 달리기는 각각 특징이 있습니다.\n극단적으로 단점만 얘기하자면 걷기는 효율이 낮고 달리기는 몸에 무리가 갑니다.\n따라서 제가 추천하는 것은 빨리걷기 입니다.",
+//                                       author: AuthorMock.author55,
+//                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 3, hour: 15, minute: 10))!,
+//                                       thumnailImageUrl: nil,
+//                                       usefulCount: 8,
+//                                       replyCount: 6)
+//    static var boardPost60 = BoardPost(id: 60,
+//                                       isAnonymous: false,
+//                                       title: "고양이가 먹이를 안먹습니다ㅠㅠ",
+//                                       content: "안녕하세요, 오늘 아침에 저희 고양이에게 먹이를 주는데 먹지를 않더라고요.. 평소에는 주자마자 허겁지겁 먹던 아이인데 이런 적은 처음이라서 당황스럽네요ㅠㅠㅠ 어디가 아픈걸까요? 혹시 이런 경험 있으신 분들 있을까요?",
+//                                       author: AuthorMock.author57,
+//                                       createdAt: Calendar.current.date(from: DateComponents(year: 2022, month: 4, day: 5, hour: 11, minute: 50))!,
+//                                       thumnailImageUrl: nil,
+//                                       usefulCount: 2,
+//                                       replyCount: 4)
 }
