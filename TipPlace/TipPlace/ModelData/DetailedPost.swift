aae2e8d68dcc73e2e90fa3200e5de929789a5c09
ddophi98 <lightcloud98@gmail.com>
2022-04-12 11:29:10 +0900

    wip: mock 데이터 형식 추가

--- TipPlace/TipPlace/ModelData/DetailedPost.swift
+++ TipPlace/TipPlace/ModelData/DetailedPost.swift
@@ -19,11 +19,15 @@ struct DetailPostModel: Codable, Identifiable{
     var tags: [String]?
     var usefulCount: Int
     var comment: [CommentModel]?
+    var idWithUseful: [Int]? // 유용해요 누른 사람들 아이디
+    var idWithReply: [Int]? // 댓글 단 사람들 아이디
+    var idWithMark: [Int]? // 북마크한 사람들 아이디
     
     init(id: Int, category: Category, isAnonymous: Bool,
          title: String, content: String, author: Author,
          createdAt: Date, images: [URL]?, tags: [String]?,
-         usefulCount: Int, comment: [CommentModel]?) {
+         usefulCount: Int, comment: [CommentModel]?, idWithUseful: [Int]?,
+         idWithReply: [Int]?, idWithMark: [Int]?) {
         self.id = id
         self.category = category
         self.isAnonymous = isAnonymous
@@ -35,5 +39,8 @@ struct DetailPostModel: Codable, Identifiable{
         self.tags = tags
         self.usefulCount = usefulCount
         self.comment = comment
+        self.idWithUseful = idWithUseful
+        self.idWithReply = idWithReply
+        self.idWithMark = idWithMark
     }
 }
