3416c22188b00f9876c6e71e72d67c80f929e5ab
skycat0212 <mrmagnet777@gmail.com>
2022-04-12 23:15:33 +0900

    refactor: 활용성이 떨어지는 코드 개선

--- TipPlace/TipPlace/DetailPost/BoxButton.swift
+++ TipPlace/TipPlace/DetailPost/BoxButton.swift
@@ -8,27 +8,21 @@
 import SwiftUI
 
 extension DetailPostView {
-    struct BoxButtonLabel: ViewModifier {
-        func body(content: Content) -> some View {
-            content
-                .font(.caption)
-                .padding(5)
-        }
-    }
-
     struct BoxButtonStyle: ButtonStyle {
         @Binding var isButtonSelected: Bool
 
         func makeBody(configuration: Self.Configuration) -> some View {
             if isButtonSelected {
                 return configuration.label
-                    .modifier(DetailPostView.BoxButtonLabel())
+                    .font(.caption)
+                    .padding(5)
                     .foregroundColor(.mint)
                     .overlay(RoundedRectangle(cornerRadius: 5)
                     .stroke(.mint, lineWidth: 1))
             } else {
                 return configuration.label
-                    .modifier(DetailPostView.BoxButtonLabel())
+                    .font(.caption)
+                    .padding(5)
                     .foregroundColor(.gray)
                     .overlay(RoundedRectangle(cornerRadius: 5)
                         .stroke(.gray, lineWidth: 1))
